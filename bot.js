9de28f5a7d67b0d5fc074611cecb803ce1ae1028
Panz1552 <43320034+Panz1552@users.noreply.github.com>
2018-09-16 19:55:11 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2305,7 +2305,7 @@ client.on('message', message => {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
-  .addField(" ** :gear: Server Support :gear: **" https://discord.gg/R9ubGU, "  ****")
+  .addField(" ** :gear: Server Support :gear: **" https://discord.gg/R9ubGU "  ****")
      
      
   message.channel.sendEmbed(embed);
